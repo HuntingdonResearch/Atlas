5e07e81acad9a2b9f45d5e8dca088a37758a6997
Umar Farooq Khawaja <umar.farooq.khawaja@gmail.com>
2022-05-23 10:50:20 +0100

    Make models data-source config work on Linux.

--- Models/users/src/config/index.ts
+++ Models/users/src/config/index.ts
@@ -1,9 +1,9 @@
-export const HOST: string = process.env.HOST || 'localhost';
+export const HOST: string = process.env.DATABASE_HOST || 'localhost';
 
-export const PORT: number = parseInt(process.env.PORT) || 13306;
+export const PORT: number = parseInt(process.env.DATABASE_PORT) || 13306;
 
-export const USERNAME: string = process.env.USERNAME || 'atlas';
+export const USERNAME: string = process.env.DATABASE_USERNAME || 'atlas';
 
-export const PASSWORD: string = process.env.PASSWORD || 'Atlas1234';
+export const PASSWORD: string = process.env.DATABASE_PASSWORD || 'Atlas1234';
 
-export const DATABASE: string = process.env.DATABASE || 'users';
+export const DATABASE: string = process.env.DATABASE_DATABASE || 'users';
