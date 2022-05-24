e453508fd3824584e2ffea59910d7df9954cb0fa
Umar Farooq Khawaja <umar.farooq.khawaja@gmail.com>
2022-05-24 18:18:34 +0100

    Externalize the following libraries: - react - react-dom - react-is - styled-components

--- Frontends/orders/src/components/App/styles.tsx
+++ Frontends/orders/src/components/App/styles.tsx
@@ -1,5 +1,4 @@
-import { keyframes } from '@emotion/react';
-import styled from '@emotion/styled';
+import styled, { keyframes } from 'styled-components';
 
 export const StyledDiv = styled.div`
   text-align: center;
